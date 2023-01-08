97f19d16e6d0cc4c4b0918b93fa4afb30c7b447b
William Merussi <williammerussi@ext.cvccorp.com.br>
2023-01-08 14:06:18 -0300

    error treatment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { MessageEnum } from './enums/message.enum';
 import { Item, Items, Meta, StockData } from './interfaces';
 
 @Component({
@@ -9,23 +10,38 @@ import { Item, Items, Meta, StockData } from './interfaces';
 export class AppComponent {
   public metaCurrency: string = '';
   public items: Item[] = [];
+  public message: string = '';
 
   constructor(private service: AppService) {}
 
   search(stockName: string) {
-    this.service.stockGet(stockName).subscribe((data: StockData) => {
-      const stockMeta: Meta | undefined = data?.meta;
-      const stockItems: Items | undefined = data?.items;
+    this.resetData();
 
-      if (!data || !stockMeta || !stockItems) {
-        return;
-      }
+    this.service.stockGet(stockName).subscribe({
+      next: (data: StockData) => {
+        const stockMeta: Meta | undefined = data?.meta;
+        const stockItems: Items | undefined = data?.items;
 
-      const itemValues = Object.values(stockItems);
-      itemValues.sort((a: Item, b: Item) => a.date_utc - b.date_utc);
+        if (!data || !stockMeta || !stockItems) {
+          this.message = MessageEnum.NO_RESULT;
+          return;
+        }
 
-      this.metaCurrency = stockMeta.currency;
-      this.items = itemValues.slice(-30);
+        const itemValues = Object.values(stockItems);
+        itemValues.sort((a: Item, b: Item) => a.date_utc - b.date_utc);
+
+        this.metaCurrency = stockMeta.currency || '';
+        this.items = itemValues.slice(-30);
+      },
+      error: () => {
+        this.message = MessageEnum.ERROR;
+      },
     });
+  }
+
+  private resetData() {
+    this.metaCurrency = '';
+    this.items = [];
+    this.message = '';
   }
 }
