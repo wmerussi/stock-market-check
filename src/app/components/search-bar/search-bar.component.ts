a6d3fe175a476731cc5c21016f7cb74a55210885
William Merussi <williammerussi@ext.cvccorp.com.br>
2023-01-08 18:37:55 -0300

    fix of app message and reusing it to display when the input field is empty

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { KeyboardEnum } from 'src/app/enums/keyboard.enum';
+import { MessageEnum } from 'src/app/enums/message.enum';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,9 +9,17 @@ import { KeyboardEnum } from 'src/app/enums/keyboard.enum';
 export class SearchBarComponent {
   @Output() onSearch: EventEmitter<string> = new EventEmitter<string>();
 
+  public message: string = '';
   public searchString: string = '';
 
   search() {
+    this.resetData();
+
+    if (!this.searchString) {
+      this.message = MessageEnum.EMPTY_INPUT;
+      return;
+    }
+
     this.onSearch.emit(this.searchString);
   }
 
@@ -18,5 +27,9 @@ export class SearchBarComponent {
     if (event.key === KeyboardEnum.ENTER) {
       this.search();
     }
+  }
+
+  private resetData() {
+    this.message = '';
   }
 }
--- src/app/enums/message.enum.ts
+++ src/app/enums/message.enum.ts
@@ -1,4 +1,5 @@
 export enum MessageEnum {
   ERROR = 'Erro inesperado, tente novamente.',
   NO_RESULT = 'Sem resultado, tente novamente.',
+  EMPTY_INPUT = 'Digite o nome da ação e tente novamente.',
 }
